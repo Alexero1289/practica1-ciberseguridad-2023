e7322e792c65a9369de3267d32f9dd0a41f0b03d
Alexero1289 <136047935+Alexero1289@users.noreply.github.com>
2023-06-18 23:17:43 -0500

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,3 @@
 user_agent = {'User-agent': 'Mozilla/5.0'}
-    url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/{symbol}?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
-    r = requests.get(url, headers=user_agent)
-
-    # IMPRIME LO DEL LINK
-    print(r.json())
+url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/{symbol}?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
+r = requests.get(url, headers=user_agent)
