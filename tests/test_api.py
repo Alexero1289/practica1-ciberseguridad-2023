faff7655a9f04a762c89e3b3f9b4815164ab5140
Alexero1289 <136047935+Alexero1289@users.noreply.github.com>
2023-06-18 23:15:39 -0500

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,2 +1,6 @@
-def test_get_price():
-    cocacola_price = get_price('KO').json
+user_agent = {'User-agent': 'Mozilla/5.0'}
+    url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/{symbol}?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
+    r = requests.get(url, headers=user_agent)
+
+    # IMPRIME LO DEL LINK
+    print(r.json())
