f50e0a83223934eb4789b9e86f6f856bee84f5b4
Alexero1289 <136047935+Alexero1289@users.noreply.github.com>
2023-06-18 23:19:27 -0500

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,4 @@
+symbol = "DIS"
 user_agent = {'User-agent': 'Mozilla/5.0'}
 url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/{symbol}?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
 r = requests.get(url, headers=user_agent)
